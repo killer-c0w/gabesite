7170164c9dc20e7e0e62fe42684944d7b8ca0d00
killer-c0w <gabe.c.kessler@gmail.com>
2024-10-25 01:36:15 +0000

    addition of tailwind css

---
+++ gabesite/app_folder/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return render_template('root.html')
+
+@app.route("/resume")
+def resume():
+    return render_template('resume.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+
+if __name__ =="__main__":
+    app.run(host='0.0.0.0',port=5000 )
